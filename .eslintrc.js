e10855be3681ce741a5fcff22cd82357a670d48f
Vũ Đức Thắng <123xxthang@gmail.com>
2025-04-04 17:25:50 +0700

    update node version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
   extends: require.resolve('@umijs/max/eslint'),
   rules: {
-    "eqeqeq": "off",                     // Tắt kiểm tra cho == và !=
-    "no-var": "off",                      // Tắt kiểm tra cho việc sử dụng var
-    "no-promise-executor-return": "off",  // Tắt kiểm tra return trong Promise executor
-    "guard-for-in": "off",               // Tắt kiểm tra cho vòng lặp for-in
-    "array-callback-return": "off",      // Tắt kiểm tra yêu cầu giá trị được trả về trong map()
-    "no-param-reassign": "off",           // Tắt kiểm tra cho việc gán giá trị cho tham số hàm
-    "react-hooks/rules-of-hooks": "off", // Tắt kiểm tra cho các React Hooks
-    "react/jsx-uses-react": "off",       // Tắt kiểm tra cho React sử dụng JSX
-    "react/jsx-uses-vars": "off"         // Tắt kiểm tra cho biến JSX
+    "no-unsafe-finally": "off",  // Tắt lỗi liên quan đến ThrowStatement và ReturnStatement
+    "no-useless-escape": "off",  // Tắt lỗi về ký tự thoát không cần thiết
+    "no-self-compare": "off",     // Tắt lỗi về việc so sánh chính nó
+    "require-yield": "off",      // Tắt lỗi yêu cầu 'yield' trong generator function
+    "no-self-assign": "off",     // Tắt lỗi về việc gán chính nó
+    "react/jsx-uses-react": "off",   // Tắt lỗi về React trong JSX
+    "react/jsx-uses-vars": "off",    // Tắt lỗi về biến trong JSX
+    "react-hooks/rules-of-hooks": "off",  // Tắt lỗi liên quan đến React Hooks
+    "eqeqeq": "off",            // Tắt lỗi liên quan đến == và !=
+    "no-var": "off",             // Tắt lỗi về việc sử dụng var
+    "no-promise-executor-return": "off",  // Tắt lỗi về return trong Promise
+    "guard-for-in": "off",       // Tắt lỗi về vòng lặp for-in
+    "array-callback-return": "off",  // Tắt lỗi yêu cầu giá trị được trả về trong map()
+    "no-param-reassign": "off"   // Tắt lỗi gán lại tham số trong hàm
   }
 };
