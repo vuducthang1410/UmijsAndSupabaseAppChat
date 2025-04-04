c620b42ce3b38bd60173bc7ab48a1a665c28d145
Vũ Đức Thắng <123xxthang@gmail.com>
2025-04-04 17:18:33 +0700

    update coding conversion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: require.resolve('@umijs/max/eslint'),
   rules: {
-    "eqeqeq": "off"
+    "eqeqeq": "off",                    // Tắt kiểm tra cho == và !=
+    "no-var": "off",                     // Tắt kiểm tra cho việc sử dụng var
+    "no-promise-executor-return": "off", // Tắt kiểm tra return trong Promise executor
+    "guard-for-in": "off",              // Tắt kiểm tra cho vòng lặp for-in
+    "array-callback-return": "off"      // Tắt kiểm tra yêu cầu giá trị được trả về trong map()
   }
 };
